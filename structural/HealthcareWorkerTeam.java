9fc963450737cb54232a6dd69db00d1bef662a41
KKudave <92803927+KKudave@users.noreply.github.com>
2022-11-09 23:02:08 +0700

    Add files via upload

---
+++ structural/HealthcareWorkerTeam.java
@@ -0,0 +1,36 @@
+package structural;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable {
+    private Set<HealthcareServiceable> members ;
+
+    public HealthcareWorkerTeam() {
+        members = new HashSet<>();
+    }
+
+    public void addMember(HealthcareServiceable member) {
+        members.add(member);
+    }
+
+    public void removeMember(HealthcareWorker member) {
+        members.remove(member);
+    }
+
+    @Override
+    public void service() {
+        for (HealthcareServiceable member : members) {
+            member.service();
+        }
+    }
+
+    @Override
+    public double getPrice() {
+        double sum_ = 0.0;
+        for (HealthcareServiceable member : members) {
+            sum_ += member.getPrice();
+        }
+        return sum_;
+    }
+}
